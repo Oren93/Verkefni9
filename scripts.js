c0c187bb7a2cf83ebc83a6905e04d9b601f000d5
Oren93 <54531718+Oren93@users.noreply.github.com>
2019-11-14 23:33:28 +0000

    Eslint+Prettier+Airbnb

--- scripts.js
+++ scripts.js
@@ -2,133 +2,137 @@ const API_URL = 'https://apis.is/company?name=';
 const error = [
   'Lén verður að vera strengur',
   'Villa við að sækja gögn',
-  'Ekkert fyrirtæki fannst fyrir leitarstreng'];
+  'Ekkert fyrirtæki fannst fyrir leitarstreng'
+];
 
-const formBtn = document.querySelector("form");
-const resultSet = document.querySelector(".results");
-const nafn = "Nafn";
+const formBtn = document.querySelector('form');
+const resultSet = document.querySelector('.results');
 
-formBtn.onsubmit = function (evt) {
+formBtn.onsubmit = function submit(evt) {
+  const ourRequest = new XMLHttpRequest();
+  let ourData;
+
+  const company = document.querySelector('input').value;
   evt.preventDefault();
-  //Clear previous data
+  // Clear previous datanpx install-peerdeps --dev eslint-config-airbnb
   resultSet.innerHTML = '';
-  var company = document.querySelector('input').value;
-  if (company == "") {
+  if (company === '') {
     errorHandler(0);
     return;
   }
   loading();
-
-  var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', API_URL + company, true);
-  ourRequest.onload = function () {
+  ourRequest.onload = function load() {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
-      var ourData = JSON.parse(ourRequest.responseText);
-      ourData = ourData.results; //simplify for later calls
+      ourData = JSON.parse(ourRequest.responseText);
+      ourData = ourData.results; // simplify for later calls
 
       resultSet.innerHTML = '';
       preRender(ourData);
       if (ourData.length === 0) {
-        resultSet.innerHTML = '';
         errorHandler(2);
-        return;
       }
     } else {
-      console.log("We connected to the server, but it returned an error.");
+      console.log('We connected to the server, but it returned an error.');
       errorHandler(1);
     }
-
   };
 
-  ourRequest.onerror = function () {
-    console.log("Connection error");
+  ourRequest.onerror = function connError() {
+    console.log('Connection error');
     resultSet.innerHTML = '';
     errorHandler(1);
-
   };
 
   ourRequest.send();
-}
+};
 
 function preRender(data) {
-  //show all active companies first, then all inactive companies
-  for (i = 0; i < data.length; i++) {
-    if (data[i].active === 1)
+  let i;
+  // Show all active companies first, then all inactive companies
+  for (i = 0; i < data.length; i += 1) {
+    if (data[i].active === 1) {
       renderHTML(data[i], true);
+    }
   }
-  for (i = 0; i < data.length; i++) {
-    if (data[i].active === 0)
+  for (i = 0; i < data.length; i += 1) {
+    if (data[i].active === 0) {
       renderHTML(data[i], false);
+    }
   }
 }
 
 function renderHTML(data, status) {
-  var infobox = document.createElement("div");
+  const infobox = document.createElement('div');
+  let elem;
+  let elemdt;
+  let elemdd;
+  const list = document.createElement('dl');
 
-  infobox.className = ((status) ? "company company--active" : "company company--inactive");
-  var list = document.createElement("dl");
-  for (var i in data)
-    console.log(i + ":\t" + data[i]);
-  console.log("\n");
+  infobox.className = status
+    ? 'company company--active'
+    : 'company company--inactive';
+  ;
+  
+  for (var i in data) console.log(i + ':\t' + data[i]);
+  console.log('\n');
 
-  //Create Nafn element
-  var nafn = document.createTextNode("Nafn");
-  var nafndt = document.createElement("dt");
-  nafndt.appendChild(nafn);
-  list.appendChild(nafndt);
-  //   }
-  //Create company name elemnt
-  var name = document.createTextNode(data.name);
-  var namedd = document.createElement("dd");
-  namedd.appendChild(name);
-  list.appendChild(namedd);
-  //Create "Kennitala" element
-  var kt = document.createTextNode("Kennitala");
-  var ktdt = document.createElement("dt");
-  ktdt.appendChild(kt);
-  list.appendChild(ktdt);
-  //Create kt number element
-  var ktno = document.createTextNode(data.sn);
-  var ktdd = document.createElement("dd");
-  ktdd.appendChild(ktno);
-  list.appendChild(ktdd);
+  // Create Nafn element
+  elem = document.createTextNode('Nafn');
+  elemdt = document.createElement('dt');
+  elemdt.appendChild(elem);
+  list.appendChild(elemdt);
+  // Create company name elemnt
+  elem = document.createTextNode(data.name);
+  elemdd = document.createElement('dd');
+  elemdd.appendChild(elem);
+  list.appendChild(elemdd);
+  // Create 'Kennitala' element
+  elem = document.createTextNode('Kennitala');
+  elemdt = document.createElement('dt');
+  elemdt.appendChild(elem);
+  list.appendChild(elemdt);
+  // Create kt number element
+  elem = document.createTextNode(data.sn);
+  elemdd = document.createElement('dd');
+  elemdd.appendChild(elem);
+  list.appendChild(elemdd);
   if (status) {
-    //Create Heimilisfang element
-    var hmf = document.createTextNode("Heimilisfang");
-    var hmfdt = document.createElement("dt");
-    hmfdt.appendChild(hmf);
-    list.appendChild(hmfdt);
-    //Create address element
-    var address = document.createTextNode(data.address);
-    var addressdd = document.createElement("dd");
-    addressdd.appendChild(address);
-    list.appendChild(addressdd);
+    // Create Heimilisfang element
+    elem = document.createTextNode('Heimilisfang');
+    elemdt = document.createElement('dt');
+    elemdt.appendChild(elem);
+    list.appendChild(elemdt);
+    // Create address element
+    elem = document.createTextNode(data.address);
+    elemdd = document.createElement('dd');
+    elemdd.appendChild(elem);
+    list.appendChild(elemdd);
   }
   infobox.appendChild(list);
   resultSet.appendChild(infobox);
-  return;
 }
 
 function errorHandler(err) {
-  //err is error-index for the array "error" defined on top
-  var errorP = document.createElement("p");
-  var errormsg = document.createTextNode(error[err])
+  const errorP = document.createElement('p');;
+  const errormsg = document.createTextNode(error[err]);;
+  // err is error-index for the array 'error' defined on top
   errorP.appendChild(errormsg);
   resultSet.appendChild(errorP);
 }
 
 function loading() {
-  //Create div wrapper with class="loading"
-  const loading = 'Leita að fyrirtækjum...';
-  var loadDiv = document.createElement("div");
-  loadDiv.className = "loading";
-  //push loading-gif into wrapper
-  var gif = document.createElement("img");
-  gif.src = "/loading.gif";
+  // Define variables on top. Create div wrapper with class='loading'
+  const loadingmsg = 'Leita að fyrirtækjum...';
+  const loadDiv = document.createElement('div');
+  const errorP = document.createElement('p');
+  const errormsg = document.createTextNode(loadingmsg);
+  const gif = document.createElement('img');
+  loadDiv.className = 'loading';
+  // push loading-gif into wrapper
+  gif.src = 'loading.gif';
   loadDiv.appendChild(gif);
-  //pus message into wrapper
-  var errorP = document.createElement("p");
-  var errormsg = document.createTextNode(loading);
+  // pus message into wrapper
   errorP.appendChild(errormsg);
   loadDiv.appendChild(errorP);
 
